5779b28f71a7d0a3f839a09814bd092d832e2ed2
Thorolund <andrey.sobol.1510@gmail.com>
2025-09-27 12:33:40 +0300

    created all tables

--- dbCode.py
+++ dbCode.py
@@ -1,6 +1,6 @@
 from sqlite3 import *
 
-con = connect("db_L")
+con = connect("db_L.db")
 curs = con.cursor()
 
 def create_tables(): #creating tables (if not exist)
@@ -9,6 +9,27 @@ def create_tables(): #creating tables (if not exist)
                     full_name TEXT NOT NULL,
                     phone TEXT NOT NULL,
                     age INTEGER NOT NULL)""")
+    
+    curs.execute("""CREATE TABLE IF NOT EXISTS books (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT NOT NULL,
+                    genre TEXT NOT NULL,
+                    total INTEGER NOT NULL CHECK(total >= 1),
+                    free INTEGER NOT NULL CHECK(free >= 0 and free <=total))""")
+    
+    curs.execute("""CREATE TABLE IF NOT EXISTS loans (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    pr TEXT NOT NULL REFERENCES readers(pr),
+                    book_id INTEGER NOT NULL REFERENCES books(id),
+                    total INTEGER NOT NULL CHECK(total >= 1),
+                    free INTEGER NOT NULL CHECK(free >= 0 and free <=total))""")
+    
+    curs.execute("""CREATE TABLE IF NOT EXISTS holds (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    pr TEXT NOT NULL REFERENCES readers(pr),
+                    book_id INTEGER NOT NULL REFERENCES books(id),
+                    total INTEGER NOT NULL CHECK(total >= 1),
+                    free INTEGER NOT NULL CHECK(free >= 0 and free <=total))""")
     
     con.commit()
     
